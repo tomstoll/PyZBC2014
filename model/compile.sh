3a2800366a23a685ccc4ff330d10aa216cac1d11
Daniel Guest <daniel_guest@urmc.rochester.edu>
2025-01-28 15:47:53 -0500

    Added requirements to .toml and added notes to compilation script

--- model/compile.sh
+++ model/compile.sh
@@ -1,8 +1,14 @@
-# Step 1: Compile model.c, complex.c, with warnings (-Wall), position independent 
-# code (-fPIC), and optimization level three (-O3)
+# This is the compilation script that I use when developing the wrapper Each .c file is
+# compiled separately and then they are linked into a shared library file called
+# `libzbc2014.so`. Flags are used as follows:
+#
+# `-c`:      Compile into object files without linking into executable
+# `-Wall`:   Enable all warnings
+# `-fPIC`:   Generate position-independent code (suitable for shared library)
+# `-03`:     Aggressively optimize
+# `-shared`: Generate a shared library instead of an executable
+
 gcc -c -Wall -fPIC -O3 complex.c 
 gcc -c -Wall -fPIC -O3 model_IHC.c
 gcc -c -Wall -fPIC -O3 model_Synapse.c
-
-# Step 2: Compile resulting objects together into a shared library 
 gcc -shared -o libzbc2014.so complex.o model_IHC.o model_Synapse.o
--- test.py
+++ test.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 # Synthesize sinusoid; 1 kHz, 50 dB SPL
 t = np.arange(0.0, 1.0, 1/100e3)
 x = np.sin(2 * np.pi * 1e3 * t)
-x = 20e-6 * 10**(0.0/20.0) * np.sqrt(2) * x
+x = 20e-6 * 10**(50.0/20.0) * np.sqrt(2) * x
 
-# Simulate IHC waveform and AN waveform, plot result
+# Simulate IHC waveform and AN waveform, plot inst. rate result
 ihcout = zbc.sim_ihc_zbc2014(x, cf=1e3)
-anout = zbc.sim_anrate_zbc2014(ihcout, cf=1e3, fibertype="lsr", noisetype="frozen")
+anout = zbc.sim_anrate_zbc2014(ihcout, cf=1e3, fibertype="msr", noisetype="none")
 plt.plot(anout)
 plt.show()
 
-# Simulate population response to pure tone
+# Simulate population average-rate response to pure tone
 cfs = np.exp(np.linspace(np.log(0.5e3), np.log(2e3), 21))
 rates = np.zeros(cfs.size)
 for i in range(cfs.size):
     ihcout = zbc.sim_ihc_zbc2014(x, cf=cfs[i])
-    anout = zbc.sim_anrate_zbc2014(ihcout, cf=cfs[i])
+    anout = zbc.sim_anrate_zbc2014(ihcout, cf=cfs[i], noisetype="none")
     rates[i] = np.mean(anout)
 
 plt.plot(cfs, rates)
